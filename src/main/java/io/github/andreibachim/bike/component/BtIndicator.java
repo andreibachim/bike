280b2704a7171e58be172dcd85f79d499853dacc
Andrei Achim <andreiachim@duck.com>
2024-11-09 12:43:29 +0200

    handle connection event

--- src/main/java/io/github/andreibachim/bike/component/BtIndicator.java
+++ src/main/java/io/github/andreibachim/bike/component/BtIndicator.java
@@ -18,86 +18,112 @@ import static io.github.andreibachim.bike.component.BtIndicator.Status.*;
 @Slf4j
 @RegisteredType(name = "BtIndicator")
 public class BtIndicator extends Button {
-    public BtIndicator(MemorySegment address) {
-        super(address);
-    }
+  public BtIndicator(MemorySegment address) {
+    super(address);
+  }
 
-    private Status status;
+  private Status status;
 
-    private void setStatus(Status status) {
-        this.status = status;
-        setIconName(status.getIconName());
-    }
+  private void setStatus(Status status) {
+    this.status = status;
+    setIconName(status.getIconName());
+  }
 
-    @InstanceInit
-    public void init() {
-        BtService.getInstance().ifPresentOrElse(btService -> {
-            if (btService.isAdapterOn()) setStatus(DISCONNECTED);
-            else setStatus(DISABLED);
-            btService.registerAdapterStateListener(new BtService.AdapterStateListener() {
-                @Override
-                public void powerOn() {
-                    setStatus(DISCONNECTED);
-                }
-
-                @Override
-                public void powerOff() {
-                    setStatus(DISABLED);
-                }
-            });
-            onClicked(() -> {
-                switch (status) {
-                    case NO_HARDWARE -> {
-                        AlertDialog dialog = new AlertDialog("No Bluetooth", "A bluetooth adapter is required to use this application.");
-                        dialog.addResponse("ok", "Ok");
-                        dialog.present(getAncestor(ApplicationWindow.getType()));
-                    }
-                    case DISABLED -> {
-                        AlertDialog dialog = new AlertDialog("Bluetooth disabled", "Bluetooth is turned off on this device.");
-                        dialog.addResponse("ok", "Ok");
-                        dialog.present(getAncestor(ApplicationWindow.getType()));
-                    }
-                    case DISCONNECTED -> disconnectedHandler(btService);
-                    case ACTIVE -> {
-                        //TODO Implemenet disconnect dialog
-                    }
-                }
-            });
-        }, () -> setStatus(NO_HARDWARE));
-    }
-
-    private void disconnectedHandler(BtService service) {
-        GtkBuilder builder = GtkBuilder
-                .fromResource("/io/github/andreibachim/bike/ui/components/dialogs/connect.ui");
-        final Dialog dialog = (Dialog) builder.getObject("connect");
-        dialog.present(getAncestor(ApplicationWindow.getType()));
-        PreferencesGroup list = ((PreferencesGroup) builder.getObject("list"));
-        try {
-            service.registerDeviceStatusListener(new BtService.DeviceStatusListener() {
-                @Override
-                public void deviceFound(BtDevice device) {
-                    list.add(new BtDeviceActionButton(device));
-                }
-
-                @Override
-                public void deviceLost(String address) {
-                    //TODO implement logic of removing device
-                }
-            });
-        } catch (Exception e) {
-            //TODO Close the window. Currently not possible to implement because this is thrown before the dialog is presented
-            ToastOverlay toastOverlay = (ToastOverlay) getAncestor(ToastOverlay.getType());
-            toastOverlay.addToast(new Toast("Could not search for new devices"));
+  @InstanceInit
+  public void init() {
+    BtService.getInstance().ifPresentOrElse(btService -> {
+      if (btService.isAdapterOn())
+        setStatus(DISCONNECTED);
+      else
+        setStatus(DISABLED);
+      btService.registerAdapterStateListener(new BtService.AdapterStateListener() {
+        @Override
+        public void powerOn() {
+          setStatus(DISCONNECTED);
         }
-    }
 
-    @Getter
-    @RequiredArgsConstructor
-    enum Status {
-        NO_HARDWARE("bluetooth-hardware-disabled"),
-        DISABLED("bluetooth-disabled"),
-        DISCONNECTED("bluetooth-disconnected"),
-        ACTIVE("bluetooth-active");
-        final String iconName;
+        @Override
+        public void powerOff() {
+          setStatus(DISABLED);
+        }
+      });
+      try {
+        btService.registerDeviceStatusListener(new BtService.DeviceStatusListener() {
+          @Override
+          public void deviceConnected(BtDevice device) {
+            setStatus(Status.ACTIVE);
+            device.getGattServices().forEach(gattService -> {
+              log.info("Service {} found with following characteristics:", gattService.getUuid());
+              gattService.getGattCharacteristics().forEach(characteristic -> {
+                log.info(characteristic.getUuid());
+              });
+            });
+          }
+
+          @Override
+          public void deviceDisconnected(BtDevice device) {
+            setStatus(Status.DISCONNECTED);
+          }
+        });
+      } catch (Exception e) {
+        // TODO error handling when no device status listener can be registered
+      }
+      onClicked(() -> {
+        switch (status) {
+          case NO_HARDWARE -> {
+            AlertDialog dialog = new AlertDialog("No Bluetooth",
+                "A bluetooth adapter is required to use this application.");
+            dialog.addResponse("ok", "Ok");
+            dialog.present(getAncestor(ApplicationWindow.getType()));
+          }
+          case DISABLED -> {
+            AlertDialog dialog = new AlertDialog("Bluetooth disabled", "Bluetooth is turned off on this device.");
+            dialog.addResponse("ok", "Ok");
+            dialog.present(getAncestor(ApplicationWindow.getType()));
+          }
+          case DISCONNECTED -> disconnectedHandler(btService);
+          case ACTIVE -> {
+            // TODO Implemenet disconnect dialog
+          }
+        }
+      });
+    }, () -> setStatus(NO_HARDWARE));
+  }
+
+  private void disconnectedHandler(BtService service) {
+    GtkBuilder builder = GtkBuilder
+        .fromResource("/io/github/andreibachim/bike/ui/components/dialogs/connect.ui");
+    final Dialog dialog = (Dialog) builder.getObject("connect");
+    dialog.present(getAncestor(ApplicationWindow.getType()));
+    PreferencesGroup list = ((PreferencesGroup) builder.getObject("list"));
+    try {
+      service.registerDeviceAvailabilityListener(new BtService.DeviceAvailabilityListener() {
+        @Override
+        public void deviceFound(BtDevice device) {
+          list.add(new BtDeviceActionButton(device));
+        }
+
+        @Override
+        public void deviceLost(String address) {
+          // TODO implement logic of removing device
+        }
+      });
+    } catch (Exception e) {
+      // TODO Close the window. Currently not possible to implement because this is
+      // thrown before the dialog is presented
+      ToastOverlay toastOverlay = (ToastOverlay) getAncestor(ToastOverlay.getType());
+      toastOverlay.addToast(new Toast("Could not search for new devices"));
     }
+  }
+
+  @Getter
+  @RequiredArgsConstructor
+  enum Status {
+    NO_HARDWARE("bluetooth-hardware-disabled"),
+    DISABLED("bluetooth-disabled"),
+    DISCONNECTED("bluetooth-disconnected"),
+    ACTIVE("bluetooth-active");
+
+    final String iconName;
+  }
 }
