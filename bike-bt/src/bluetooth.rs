b0be129f4c350e6490fd24ce973a780528167a16
Andrei Achim <andreiachim@duck.com>
2025-02-13 18:39:25 +0200

    refactor bluetooth lib to not use specific tokio runtime

--- bike-bt/src/bluetooth.rs
+++ bike-bt/src/bluetooth.rs
@@ -1,130 +1,67 @@
-use std::sync::Arc;
-
 use crate::{BluetoothError, BluetoothStatus, Device, DeviceDiscoveryEvent};
 use bluer::{
-    Adapter,
     AdapterEvent::{DeviceAdded, DeviceRemoved, PropertyChanged},
     Address,
 };
 use futures::{stream::StreamExt, Stream};
 
 pub struct BikeBt {
-    runtime: tokio::runtime::Runtime,
-    pub adapter: Arc<bluer::Adapter>,
+    pub session: bluer::Session,
+    pub adapter: bluer::Adapter,
 }
 
 impl BikeBt {
     pub async fn new() -> Result<Self, BluetoothError> {
-        let runtime = tokio::runtime::Runtime::new().map_err(|_| BluetoothError::NoBluez)?;
-        let adapter = runtime
-            .spawn(async {
-                let session = bluer::Session::new()
-                    .await
-                    .map_err(|_| BluetoothError::NoBluez)?;
-                let adapter = session
-                    .default_adapter()
-                    .await
-                    .map_err(|_| BluetoothError::NoAdapter)?;
-                Ok::<Adapter, BluetoothError>(adapter)
-            })
+        let session = bluer::Session::new()
             .await
-            .map_err(|_| BluetoothError::NoAdapter)??;
-        Ok(Self {
-            adapter: Arc::new(adapter),
-            runtime,
-        })
+            .map_err(|_| BluetoothError::NoBluez)?;
+
+        let adapter = session
+            .default_adapter()
+            .await
+            .map_err(|_| BluetoothError::NoAdapter)?;
+        Ok(Self { session, adapter })
     }
 
     pub async fn get_status(&self) -> BluetoothStatus {
-        let adapter = self.adapter.clone();
-        self.runtime
-            .spawn(async move {
-                adapter.is_powered().await.map_or_else(
-                    |_| BluetoothStatus::Unavailable,
-                    |value| {
-                        if value {
-                            BluetoothStatus::Disconnected
-                        } else {
-                            BluetoothStatus::PoweredOff
-                        }
-                    },
-                )
-            })
-            .await
-            .unwrap_or(BluetoothStatus::Unavailable)
+        self.adapter.is_powered().await.map_or_else(
+            |_| BluetoothStatus::Unavailable,
+            |powered| {
+                if powered {
+                    BluetoothStatus::Disconnected
+                } else {
+                    BluetoothStatus::PoweredOff
+                }
+            },
+        )
     }
 
     pub async fn scan(
         &self,
     ) -> Result<impl Stream<Item = DeviceDiscoveryEvent> + use<'_>, BluetoothError> {
-        let adapter = self.adapter.clone();
         let stream = self
-            .runtime
-            .spawn(async move {
-                let stream = adapter
-                    .discover_devices_with_changes()
-                    .await
-                    .map_err(|_| BluetoothError::ScanFailed)?
-                    .filter_map(move |item| {
-                        let adapter = adapter.clone();
-                        async move {
-                            match item {
-                                DeviceAdded(address) => {
-                                    let device = BikeBt::get_device(adapter, address).await;
-                                    match device {
-                                        Ok(device) => {
-                                            Some(DeviceDiscoveryEvent::DeviceAdded(device))
-                                        }
-                                        Err(_) => None,
-                                    }
-                                }
-                                DeviceRemoved(address) => {
-                                    let device = BikeBt::get_device(adapter, address).await;
-                                    match device {
-                                        Ok(device) => {
-                                            Some(DeviceDiscoveryEvent::DeviceRemoved(device))
-                                        }
-                                        Err(_) => None,
-                                    }
-                                }
-                                PropertyChanged(_) => None,
-                            }
-                        }
-                    });
-                Ok::<_, BluetoothError>(stream)
-            })
+            .adapter
+            .discover_devices()
             .await
-            .map_err(|_| BluetoothError::ScanFailed)??;
-        Ok(stream)
-
-        //
-        //Ok(adapter
-        //    .discover_devices_with_changes()
-        //    .await
-        //    .map_err(|_| BluetoothError::ScanFailed)?
-        //    .filter_map(move |item| async move {
-        //        match item {
-        //            DeviceAdded(address) => {
-        //                let device = self.get_device(address).await;
-        //                match device {
-        //                    Ok(device) => Some(DeviceDiscoveryEvent::DeviceAdded(device)),
-        //                    Err(_) => None,
-        //                }
-        //            }
-        //            DeviceRemoved(address) => {
-        //                let device = self.get_device(address).await;
-        //                match device {
-        //                    Ok(device) => Some(DeviceDiscoveryEvent::DeviceRemoved(device)),
-        //                    Err(_) => None,
-        //                }
-        //            }
-        //            PropertyChanged(_) => None,
-        //        }
-        //    }))
+            .map_err(|_| BluetoothError::ScanFailed)?
+            .filter_map(move |item| async move {
+                match item {
+                    DeviceAdded(address) => match self.get_device(address).await {
+                        Ok(device) => Some(DeviceDiscoveryEvent::DeviceAdded(device)),
+                        Err(_) => None,
+                    },
+                    DeviceRemoved(address) => match self.get_device(address).await {
+                        Ok(device) => Some(DeviceDiscoveryEvent::DeviceRemoved(device)),
+                        Err(_) => None,
+                    },
+                    PropertyChanged(_) => None,
+                }
+            });
+        Ok::<_, BluetoothError>(stream)
     }
 
-    async fn get_device(adapter: Arc<Adapter>, address: Address) -> Result<Device, ()> {
-        let device = adapter.device(address).map_err(|_| ())?;
+    async fn get_device(&self, address: Address) -> Result<Device, ()> {
+        let device = self.adapter.device(address).map_err(|_| ())?;
         let name = device.name().await.map_err(|_| ())?.ok_or(())?;
         let rssi = device.rssi().await.ok().flatten().unwrap_or(-101_i16);
         Ok(Device::new(address.to_string(), name, rssi))
