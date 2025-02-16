d9311515fa678ff360691115c7c9bd21ecfc00c7
Andrei Achim <andreiachim@duck.com>
2025-02-16 21:49:34 +0200

    stub out viewstack navigation

--- bike-ui/src/components/app.rs
+++ bike-ui/src/components/app.rs
@@ -1,6 +1,7 @@
 use bike_bt::BikeBt;
 use relm4::{
-    adw::{prelude::AdwApplicationWindowExt, HeaderBar},
+    adw::{prelude::AdwApplicationWindowExt, HeaderBar, ViewStack, ViewSwitcher},
+    gtk::{prelude::BoxExt, Label},
     prelude::{
         AsyncComponent, AsyncComponentController, AsyncComponentParts, AsyncController,
         SimpleAsyncComponent,
@@ -57,6 +58,34 @@ impl SimpleAsyncComponent for App {
         //Setup the toolbar view
         let toolbar_view = relm4::adw::ToolbarView::new();
         toolbar_view.add_top_bar(&header_bar);
+
+        let content = relm4::gtk::Box::new(relm4::gtk::Orientation::Vertical, 10);
+        let view_stack = ViewStack::builder().build();
+        let view_switcher = ViewSwitcher::builder()
+            .halign(relm4::gtk::Align::Center)
+            .hexpand(false)
+            .policy(relm4::adw::ViewSwitcherPolicy::Wide)
+            .stack(&view_stack)
+            .build();
+        header_bar.set_title_widget(Some(&view_switcher));
+
+        //Add a couple of pages
+        view_stack.add_titled_with_icon(
+            &Label::new(Some("")),
+            Some("routes"),
+            "Routes",
+            "org.gnome.Maps-symbolic",
+        );
+        view_stack.add_titled_with_icon(
+            &Label::new(Some("")),
+            Some("history"),
+            "History",
+            "accessories-text-editor",
+        );
+
+        content.append(&view_stack);
+
+        toolbar_view.set_content(Some(&content));
 
         //Content
         root.set_content(Some(&toolbar_view));
