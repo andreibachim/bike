ff0e7b23ccba3d27b50f3a0080eb3d8d5cf1e1b3
Andrei Achim <andreiachim@duck.com>
2025-03-21 14:02:10 +0200

    validation to ensure at least one adapter is present

--- src/bluetooth/service.rs
+++ src/bluetooth/service.rs
@@ -2,26 +2,32 @@ use std::collections::HashMap;
 
 use gtk::{
     gio::{BusType, Cancellable, DBusCallFlags, DBusConnection, DBusError, DBusSignalFlags},
-    glib::{Variant, VariantTy, variant::ToVariant},
+    glib::{
+        Variant, VariantTy,
+        variant::{ObjectPath, ToVariant},
+    },
 };
 
 const BLUEZ_BUS_NAME: Option<&str> = Some("org.bluez");
-const ADAPTER_OBJECT_PATH: Option<&str> = Some("/org/bluez/hci0");
-const ADAPTER_INTERFACE: Option<&str> = Some("org.bluez.Adapter1");
-const PROPERTIES_INTERFACE: Option<&str> = Some("org.freedesktop.DBus.Properties");
+const ADAPTER_OBJECT_PATH: &str = "/org/bluez/hci0";
+const ADAPTER_INTERFACE: &str = "org.bluez.Adapter1";
+const PROPERTIES_INTERFACE: &str = "org.freedesktop.DBus.Properties";
 
 pub struct BluetoothService {
     connection: Result<DBusConnection, gtk::glib::Error>,
+    adapter_object_path: Option<String>,
 }
 
 impl BluetoothService {
     pub fn new() -> Self {
         let connection = gtk::gio::bus_get_sync(BusType::System, Cancellable::NONE);
-        Self { connection }
+        let mut slf = Self { connection, adapter_object_path: None };
+        slf.adapter_object_path = slf.get_adapters().first().map(|object_path| object_path.to_string());
+        slf
     }
 
     pub fn is_valid(&self) -> bool {
-        self.connection.is_ok()
+        self.connection.is_ok() && self.adapter_object_path.is_some()
     }
 
     pub fn is_adapter_powered(&self) -> Result<bool, gtk::glib::Error> {
@@ -34,16 +40,10 @@ impl BluetoothService {
 
         let powered_variant = &self.connection.clone()?.call_sync(
             BLUEZ_BUS_NAME,
-            ADAPTER_OBJECT_PATH.expect("Adapter object path is not defined"),
-            PROPERTIES_INTERFACE.expect("Properties interface name is not defined"),
+            ADAPTER_OBJECT_PATH,
+            PROPERTIES_INTERFACE,
             "Get",
-            Some(
-                &(
-                    ADAPTER_INTERFACE.expect("Adapter interface name is not defined"),
-                    "Powered",
-                )
-                    .to_variant(),
-            ),
+            Some(&(ADAPTER_INTERFACE, "Powered").to_variant()),
             Some(VariantTy::ANY),
             DBusCallFlags::NONE,
             300,
@@ -68,10 +68,10 @@ impl BluetoothService {
         if let Ok(connection) = &self.connection {
             connection.signal_subscribe(
                 BLUEZ_BUS_NAME,
-                PROPERTIES_INTERFACE,
+                Some(PROPERTIES_INTERFACE),
                 Some("PropertiesChanged"),
-                ADAPTER_OBJECT_PATH,
-                ADAPTER_INTERFACE,
+                Some(ADAPTER_OBJECT_PATH),
+                Some(ADAPTER_INTERFACE),
                 DBusSignalFlags::NONE,
                 move |_, _, _, _, _, value| {
                     let _ = value
@@ -85,6 +85,39 @@ impl BluetoothService {
                         });
                 },
             );
+        }
+    }
+
+    fn get_adapters(&self) -> Vec<ObjectPath> {
+        if let Ok(connection) = &self.connection {
+            connection
+                .call_sync(
+                    BLUEZ_BUS_NAME,
+                    "/",
+                    "org.freedesktop.DBus.ObjectManager",
+                    "GetManagedObjects",
+                    None,
+                    Some(VariantTy::ANY),
+                    DBusCallFlags::NONE,
+                    3000,
+                    Cancellable::NONE,
+                )
+                .unwrap_or(
+                    (HashMap::<
+                        ObjectPath,
+                        HashMap<String, HashMap<String, Variant>>,
+                    >::new(),)
+                        .to_variant(),
+                )
+                .get::<(HashMap<ObjectPath, HashMap<String, HashMap<String, Variant>>>,)>()
+                .unwrap_or((HashMap::new(),))
+                .0
+                .into_iter()
+                .filter(|(_, v)| v.contains_key(ADAPTER_INTERFACE))
+                .map(|(k, _)| k)
+                .collect()
+        } else {
+            vec![]
         }
     }
 }
